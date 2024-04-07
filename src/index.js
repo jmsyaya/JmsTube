8c8ab6767443f8294bc7d9b05a66652888e53174
JeongMyeongSik <jmsyaya@naver.com>
2024-04-08 01:21:59 +0900

    SCSS Setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './assets/scss/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));  // root는 index.html로 뿌려줌.
 root.render(
