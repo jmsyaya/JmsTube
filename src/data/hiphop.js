5ea1a2e87fd1771c3d54a52d405e9a0cfe1389ac
JeongMyeongSik <jmsyaya@naver.com>
2024-04-30 01:35:17 +0900

    DB 1차 수정

--- src/data/hiphop.js
+++ src/data/hiphop.js
@@ -1,121 +1,121 @@
-import gsap01 from "../assets/img/gsap/gsap01.jpg";
-import gsap02 from "../assets/img/gsap/gsap02.jpg";
-import gsap03 from "../assets/img/gsap/gsap03.jpg";
-import gsap04 from "../assets/img/gsap/gsap04.jpg";
-import gsap05 from "../assets/img/gsap/gsap05.jpg";
-import gsap06 from "../assets/img/gsap/gsap06.jpg";
-import gsap07 from "../assets/img/gsap/gsap07.jpg";
-import gsap08 from "../assets/img/gsap/gsap08.jpg";
-import gsap09 from "../assets/img/gsap/gsap09.jpg";
-import gsap10 from "../assets/img/gsap/gsap10.jpg";
-import gsap11 from "../assets/img/gsap/gsap11.jpg";
-import gsap12 from "../assets/img/gsap/gsap12.jpg";
-import gsap13 from "../assets/img/gsap/gsap13.jpg";
-import gsap14 from "../assets/img/gsap/gsap14.jpg";
-import gsap15 from "../assets/img/gsap/gsap15.jpg";
+import rap01 from "../assets/img/hiphop/rap01.jpg";
+import rap02 from "../assets/img/hiphop/rap02.jpg";
+import rap03 from "../assets/img/hiphop/rap03.jpg";
+import rap04 from "../assets/img/hiphop/rap04.jpg";
+import rap05 from "../assets/img/hiphop/rap05.jpg";
+import rap06 from "../assets/img/hiphop/rap06.jpg";
+import rap07 from "../assets/img/hiphop/rap07.jpg";
+import rap08 from "../assets/img/hiphop/rap08.jpg";
+import rap09 from "../assets/img/hiphop/rap09.jpg";
+import rap10 from "../assets/img/hiphop/rap10.jpg";
+import rap11 from "../assets/img/hiphop/rap11.jpg";
+import rap12 from "../assets/img/hiphop/rap12.jpg";
+import rap13 from "../assets/img/hiphop/rap13.jpg";
+import rap14 from "../assets/img/hiphop/rap14.jpg";
+import rap15 from "../assets/img/hiphop/rap15.jpg";
 
 export const hiphopText = [
     {
-        title: "01. GSAP Parallax Effect : 기본 애니메이션(feat. scrollTrigger) |  웹스토리보이",
-        img: gsap01,
-        author: "webstoryboy",
-        videoId: "_rAAY_Zp4FI",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "김하온 (HAON) - 꼴통 (feat. 창모 (CHANGMO)) [HAONOAH]ㅣLyrics/가사",
+        img: rap01,
+        author: "알파",
+        videoId: "Metzq5MbeVs",
+        date: "2024.03.29",
+        channelId: "UCGuE0TrBNIZKfordUwAQe2w",
     },{
-        title: "02. GSAP Parallax Effect : PIN 애니메이션(feat. scrollTrigger) | 웹스토리보이 |  포트폴리오",
-        img: gsap02,
-        author: "webstoryboy",
-        videoId: "T9kkvtHaq8E",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[4K] Men's Tear(맨스티어) - AK47 Official M/V",
+        img: rap02,
+        author: "뷰티풀너드",
+        videoId: "QW_TSknSbWM",
+        date: "2024.02.28",
+        channelId: "UCe9f9MVu4WVCA3tVWtgSswA",
     },{
-        title: "03. GSAP Parallax Effect : PIN 배경 고정하기(feat. scrollTrigger) | 웹스토리보이 |  스크롤 효과",
-        img: gsap03,
-        author: "webstoryboy",
-        videoId: "bP7iGE3YKOU",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "♬ 위하여(We Higher) (Feat. 릴보이) - 블라세, 노윤하, 폴로다레드, 칠린호미, 플리키뱅 ",
+        img: rap03,
+        author: "GROOVYROOM",
+        videoId: "NMd7u0TEWHU",
+        date: "2022.12.03",
+        channelId: "UCSOCNcBfiByb4cr3LLL8V8w",
     },{
-        title: "04. GSAP Parallax Effect : 이질감 효과(feat. scrollTrigger) | 웹스토리보이 |  스크롤 효과",
-        img: gsap04,
-        author: "webstoryboy",
-        videoId: "neBcJlgfu_o",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "릴타치 (Lil tachi), 영비 (Young B), 윤훼이 (YUNHWAY), 저스디스 (JUSTHIS) - 나쁜맛 [Cumpilation]ㅣLyrics/가사",
+        img: rap04,
+        author: "알파",
+        videoId: "5Llz1UmXF2Y",
+        date: "2019.12.29",
+        channelId: "UCGuE0TrBNIZKfordUwAQe2w",
     },{
-        title: "05. GSAP Parallax Effect : 나타나기 효과(feat. scrollTrigger) | 웹스토리보이 | 포폴 이펙트",
-        img: gsap05,
-        author: "webstoryboy",
-        videoId: "9lnfRXTXzL0",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "토일 - 너 포에버 Megamix (Feat. 염따, 머쉬베놈, UNEDUCATED KID, CHANGMO, Northfacegawd, 던밀스) [DF LIVE] TOIL",
+        img: rap05,
+        author: "dingo freestyle",
+        videoId: "JIUSTeuEMaY",
+        date: "2021.07.05",
+        channelId: "UCviI9lzTe2pkxJ9M2ArA7WQ",
     },{
-        title: "06. GSAP Parallax Effect : 텍스트 효과(feat. scrollTrigger) | 웹스토리보이 | 텍스트 이펙트",
-        img: gsap06,
-        author: "webstoryboy",
-        videoId: "gzJnKZGsB-Y",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "박재범, 슬롬 - BLUE CHECK REMIX (Feat. 블라세, 365LIT, 주피터, 칸, 신세인, 노윤하, 소코도모, 코알라, 잠비노, 이영지, 칠린호미, NSWyoon)",
+        img: rap06,
+        author: "dingo freestyle",
+        videoId: "1GbbAUQB-ZM",
+        date: "2023.01.20",
+        channelId: "UCviI9lzTe2pkxJ9M2ArA7WQ",
     },{
-        title: "07. GSAP Parallax Effect : 배경색 효과(feat. scrollTrigger) | 웹스토리보이 | 배경 바꾸기",
-        img: gsap07,
-        author: "webstoryboy",
-        videoId: "kE0QRWPBVKs",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "Fleeky Bang ft. Tray B - MY NAME IS [Official Music Video]",
+        img: rap07,
+        author: "DIRTY PLAY RECORDS",
+        videoId: "xNR44gSDlLQ",
+        date: "2024.01.25",
+        channelId: "UCq162naAcWTUHxsc_z-AuBg",
     },{
-        title: "08. GSAP Parallax Effect : 진행바 효과(feat. scrollTrigger) | 웹스토리보이 | progress",
-        img: gsap08,
-        author: "webstoryboy",
-        videoId: "pLgDSor_27g",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "Polodared ft. MIRANI - 베이베 (BABY) [Official Audio]",
+        img: rap08,
+        author: "DIRTY PLAY RECORDS",
+        videoId: "0vY-GRMBTgc",
+        date: "2024.02.14",
+        channelId: "UCq162naAcWTUHxsc_z-AuBg",
     },{
-        title: "09. GSAP Parallax Effect : 메뉴 이동 효과 (feat. scrollTrigger) | 웹스토리보이 | 메뉴 클릭하기",
-        img: gsap09,
-        author: "webstoryboy",
-        videoId: "d5xSXS1UZFY",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "119 REMIX (Prod.GRAY) - Various Artists",
+        img: rap09,
+        author: "AOMGOFFICIAL",
+        videoId: "gxvpq3-D1HI",
+        date: "2018.12.04",
+        channelId: "UCW5gbSGTGIukUDsPt2sdhGw",
     },{
-        title: "10. GSAP Parallax Effect : 메뉴 축소 효과 (feat. scrollTrigger) | 웹스토리보이 | 메뉴 바꾸기",
-        img: gsap10,
-        author: "webstoryboy",
-        videoId: "Z2NeyjasCdY",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "10. Connor Price - Violet (feat. Killa) [Official Audio]",
+        img: rap10,
+        author: "Connor Price",
+        videoId: "GcvSJudQbVs",
+        date: "2022.10.08",
+        channelId: "UCDzdCE9XbxTbgvuVdhbLtOQ",
     },{
-        title: "11. GSAP Parallax Effect : 메뉴 숨기기 효과 (feat. scrollTrigger) | 웹스토리보이 | 메뉴 바꾸기",
-        img: gsap11,
-        author: "webstoryboy",
-        videoId: "zMBXXAl7MaY",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "Armani White - BILLIE EILISH.",
+        img: rap11,
+        author: "Armani White",
+        videoId: "4vYOwhll1fs",
+        date: "2022.05.24",
+        channelId: "UCf1fMyfaP3YYkiAPq7LyEnw",
     },{
         title: "12. GSAP Parallax Effect : 가로 효과 (feat. scrollTrigger) | 웹스토리보이 | 가로 사이트",
-        img: gsap12,
+        img: rap12,
         author: "webstoryboy",
         videoId: "DCtCgE08E8I",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
         title: "13. GSAP Parallax Effect : 가로/세로 효과 (feat. scrollTrigger) | 웹스토리보이 | 가로세로 사이트",
-        img: gsap13,
+        img: rap13,
         author: "webstoryboy",
         videoId: "YPzwvzFfCfU",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
         title: "14. GSAP Parallax Effect : 가로/세로 나타나기 효과 (feat. scrollTrigger) | 웹스토리보이 | 포폴사이트",
-        img: gsap14,
+        img: rap14,
         author: "webstoryboy",
         videoId: "puCe3ollwTg",
         date: "2023.09.09",
         channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
     },{
         title: "15. GSAP Parallax Effect : 스무스 효과 (feat. scrollTrigger) | 웹스토리보이 | 포폴은 스무스지",
-        img: gsap15,
+        img: rap15,
         author: "webstoryboy",
         videoId: "tMsFs6GoXmg",
         date: "2023.09.09",
--- src/data/kpop.js
+++ src/data/kpop.js
@@ -1,25 +1,28 @@
 import kpop01 from "../assets/img/kpop/site01.jpg";
-import website02 from "../assets/img/kpop/site02.jpg";
-import website03 from "../assets/img/kpop/site03.jpg";
-import website04 from "../assets/img/kpop/site04.jpg";
-import website05 from "../assets/img/kpop/site05.jpg";
-import website06 from "../assets/img/kpop/site06.jpg";
-import website07 from "../assets/img/kpop/site07.jpg";
-import website08 from "../assets/img/kpop/site08.jpg";
-import website09 from "../assets/img/kpop/site09.jpg";
-import website10 from "../assets/img/kpop/site10.jpg";
-import website11 from "../assets/img/kpop/site11.jpg";
-import website12 from "../assets/img/kpop/site12.jpg";
-import website13 from "../assets/img/kpop/site13.jpg";
-import website14 from "../assets/img/kpop/site14.jpg";
-import website15 from "../assets/img/kpop/site15.jpg";
-import website16 from "../assets/img/kpop/site16.jpg";
-import website17 from "../assets/img/kpop/site17.jpg";
-import website18 from "../assets/img/kpop/site18.jpg";
-import website19 from "../assets/img/kpop/site19.jpg";
-import website20 from "../assets/img/kpop/site20.jpg";
-import website21 from "../assets/img/kpop/site21.jpg";
-import website22 from "../assets/img/kpop/site22.jpg";
+import kpop02 from "../assets/img/kpop/site02.jpg";
+import kpop03 from "../assets/img/kpop/site03.jpg";
+import kpop04 from "../assets/img/kpop/site04.jpg";
+import kpop05 from "../assets/img/kpop/site05.jpg";
+import kpop06 from "../assets/img/kpop/site06.jpg";
+import kpop07 from "../assets/img/kpop/site07.jpg";
+import kpop08 from "../assets/img/kpop/site08.jpg";
+import kpop09 from "../assets/img/kpop/site09.jpg";
+import kpop10 from "../assets/img/kpop/site10.jpg";
+import kpop11 from "../assets/img/kpop/site11.jpg";
+import kpop12 from "../assets/img/kpop/site12.jpg";
+import kpop13 from "../assets/img/kpop/site13.jpg";
+import kpop14 from "../assets/img/kpop/site14.jpg";
+import kpop15 from "../assets/img/kpop/site15.jpg";
+import kpop16 from "../assets/img/kpop/site16.jpg";
+import kpop17 from "../assets/img/kpop/site17.jpg";
+import kpop18 from "../assets/img/kpop/site18.jpg";
+import kpop19 from "../assets/img/kpop/site19.jpg";
+import kpop20 from "../assets/img/kpop/site20.jpg";
+import kpop21 from "../assets/img/kpop/site21.jpg";
+import kpop22 from "../assets/img/kpop/site22.jpg";
+import kpop23 from "../assets/img/kpop/site23.jpg";
+import kpop24 from "../assets/img/kpop/site24.jpg";
+import kpop25 from "../assets/img/kpop/site25.jpg";
 
 export const kpopText = [
     {
@@ -30,158 +33,175 @@ export const kpopText = [
         date: "2024.04.03",
         channelId: "UCh21enuxt6axA0nkT6y4J2Q",
     },{
-        title: "02. 웹표준 사이트 만들기 : 디자인 : 슬라이드 유형01 / 피그마(Figma)로 작업하기 / 웹스토리보이",
-        img: website02,
-        author: "webstoryboy",
-        videoId: "Qs4viazDmdU",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "ILLIT(아일릿) - Magnetic (4K) | ILLIT : I'LL (SHOW) IT | Mnet 240325 방송",
+        img: kpop02,
+        author: "M2",
+        videoId: "TEKyEQL-S8o",
+        date: "2024.03.25",
+        channelId: "UCTQVIXvcHrR9jYoJ6qaBAow",
     },{
-        title: "03. 웹표준 사이트 만들기 : 디자인 : 이미지 유형01 / 피그마(Figma)로 작업하기 / 웹사이트 만들기",
-        img: website03,
-        author: "webstoryboy",
-        videoId: "yh4MUO05ils",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "TWS (투어스) '첫 만남은 계획대로 되지 않아' Official MV",
+        img: kpop03,
+        author: "HYBE LABELS",
+        videoId: "hVAc1Vf2ITU",
+        date: "2024.01.22",
+        channelId: "UC3IZKseVpdzPSBaWxBxundA",
     },{
-        title: "04. 웹표준 사이트 만들기 : 디자인 : 이미지/텍스트 유형01 / 피그마(Figma)로 작업하기 / 사이트 만들기",
-        img: website04,
-        author: "webstoryboy",
-        videoId: "nxpQq7wM7m0",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[MV] BABY BLUE(베이비블루) _ Would You Smile(웃어줄래)",
+        img: kpop04,
+        author: "1theK (원더케이)",
+        videoId: "aZwxC8RV0SA",
+        date: "2023.03.19",
+        channelId: "UCweOkPb1wVVH0Q0Tlj4a5Pw",
     },{
-        title: "05. 웹표준 사이트 만들기 : 디자인 : 카드 유형01 / 피그마(Figma)로 작업하기 / 나도코딩",
-        img: website05,
-        author: "webstoryboy",
-        videoId: "hN_oia3ftOk",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[LIVE] 이창섭 - 천상연 (선녀외전 OST) 라이브 (Full. ver)",
+        img: kpop05,
+        author: "TOON STUDIO",
+        videoId: "fQ84rvbfa08",
+        date: "2024.02.21",
+        channelId: "UCve0b6qhfxTpKeqzZ4u0MjQ",
     },{
-        title: "06. 웹표준 사이트 만들기 : 디자인 : 배너 유형01 / 피그마(Figma)로 작업하기 / 코딩 기초",
-        img: website06,
-        author: "webstoryboy",
-        videoId: "-JEzrg5yg9g",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "순순희(지환) - 슬픈 초대장",
+        img: kpop06,
+        author: "순순희",
+        videoId: "hkROGL9id6Y",
+        date: "2023.12.20",
+        channelId: "UCxVJ3rXFKib5atFxUMc35fw",
     },{
-        title: "07. 웹표준 사이트 만들기 : 디자인 : 텍스트 유형01 / 피그마(Figma)로 작업하기 / 코딩 독학",
-        img: website07,
-        author: "webstoryboy",
-        videoId: "zN4kK-RjFXE",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[4K] (여자)아이들((G)I-DLE) “나는 아픈 건 딱 질색이니까(Fate)” Band LIVE Concert",
+        img: kpop07,
+        author: "it's Live",
+        videoId: "JIMr1p-P_ms",
+        date: "2024.02.15",
+        channelId: "UCB9e3pof1o83aa0kkaoeJGA",
     },{
-        title: "08. 웹표준 사이트 만들기 : 디자인 : 푸터 유형01  / 피그마(Figma)로 작업하기 / 피그마 강의",
-        img: website08,
-        author: "webstoryboy",
-        videoId: "Wu2piDp007M",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "DAY6(데이식스) '한 페이지가 될 수 있게' M/V",
+        img: kpop08,
+        author: "JYP Entertainment",
+        videoId: "vnS_jn2uibs",
+        date: "2019.07.15",
+        channelId: "UCaO6TYtlC8U5ttz62hTrZgg",
     },{
-        title: "09. 웹표준 사이트 만들기 : 디자인 : 완성 / 피그마(Figma)로 작업하기 / 피그마 기초",
-        img: website09,
-        author: "webstoryboy",
-        videoId: "2CPRqp21mWM",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[MV] LEE MU JIN(이무진) _ Episode(에피소드)",
+        img: kpop09,
+        author: "1theK (원더케이)",
+        videoId: "gH1rVdXUlpo",
+        date: "2023.12.13",
+        channelId: "UCweOkPb1wVVH0Q0Tlj4a5Pw",
     },{
-        title: "10. 웹표준 사이트 만들기 : 레이아웃 : 레이아웃 유형01 / 홈페이지 만들기 / 웹스토리보이",
-        img: website10,
-        author: "webstoryboy",
-        videoId: "qnwa6I5WosI",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "BIBI (비비) - Bam Yang Gang | Show! MusicCore | MBC240217방송",
+        img: kpop10,
+        author: "MBCkpop",
+        videoId: "CqBTZ9uku8s",
+        date: " 2024.02.17",
+        channelId: "UCe52oeb7Xv_KaJsEzcKXJJg",
     },{
-        title: "11. 웹표준 사이트 만들기 : 레이아웃 : 레이아웃 유형02 / 웹사이트 만들기 / HTML / CSS",
-        img: website11,
-        author: "webstoryboy",
-        videoId: "dNQZqNd_uUE",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "SPOT! - 지코 (ZICO) [더 시즌즈-지코의 아티스트] | KBS 240426 방송",
+        img: kpop11,
+        author: "KBS kpop",
+        videoId: "nEC_ntsw540",
+        date: "2024.04.27",
+        channelId: "UCeLPm9yH_a_QH8n6445G-Ow",
     },{
-        title: "12. 웹표준 사이트 만들기 : 레이아웃 : 레이아웃 유형03 / 웹사이트 만들기 / JavaScript / CSS",
-        img: website12,
-        author: "webstoryboy",
-        videoId: "uh8gxb3f8gA",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "치즈 (CHEEZE) - 무드 인디고 (Mood Indigo) [MV]",
+        img: kpop12,
+        author: "MAGIC STRAWBERRY SOUND",
+        videoId: "LsLUz7ArmGI",
+        date: "2016.05.26",
+        channelId: "UCnmF6Fy8t1aIDl5jlLqSAqw",
     },{
-        title: "13. 웹표준 사이트 만들기 : 레이아웃 : 레이아웃 유형04 / 웹사이트 만들기 / JavaScript / CSS",
-        img: website13,
-        author: "webstoryboy",
-        videoId: "q4_4J_qt-As",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "Confession (Ditto X Chuu (LOONA)) (고백 (영화 '동감' X 츄 (이달의 소녀)))",
+        img: kpop13,
+        author: "츄-주제",
+        videoId: "Flu8jHjwBx0",
+        date: "2022.10.16",
+        channelId: "UCWBGeXdzpTbm6Pd6_1lFVGg",
     },{
-        title: "14. 웹표준 사이트 만들기 : 레이아웃 : 레이아웃 유형05 / 웹사이트 만들기 / Grid / Flex / Float",
-        img: website14,
-        author: "webstoryboy",
-        videoId: "MFGsZYECt0Q",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[LIVE CLIP] 케이시(Kassy)_'사실말야내가말야그게그러니까말이야' (Crush on you)",
+        img: kpop14,
+        author: "케이시 Kassy Official",
+        videoId: "P4fK4bTxBjA",
+        date: "2023.06.05",
+        channelId: "UCfxrNQQDxLhBoOeOZeUolbQ",
     },{
-        title: "15. 웹표준 사이트 만들기 : 코딩 : 헤더 유형01 / 웹사이트 만들기 / 웹퍼블리셔 / 포트폴리오",
-        img: website15,
-        author: "webstoryboy",
-        videoId: "y2O1zykEyFQ",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "데이식스 (DAY6) '예뻤어 (You Were Beautiful)' 교차편집 (Stage Mix)",
+        img: kpop15,
+        author: "호구마 HOGUMA",
+        videoId: "I3U0QAXeOW4",
+        date: "2018.10.13",
+        channelId: "UC07i8JgLI1h8ORHEBlnQE2A",
     },{
-        title: "16. 웹표준 사이트 만들기 : 코딩 : 슬라이드 유형01 / 웹사이트 만들기 / 웹 퍼블리셔 / 포트폴리오",
-        img: website16,
-        author: "webstoryboy",
-        videoId: "jjHLaeG4BCs",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "전화 한 번 못하니 (너에게 하고 싶은 말 X 신예영) 가로 Live Full ver.",
+        img: kpop16,
+        author: "리본 프로젝트 - Re:born project",
+        videoId: "n-LezuGtBHo",
+        date: "2021.09.28",
+        channelId: "UC_aGbdBiH9vMpiJ1zHZ_gxg",
     },{
-        title: "17. 웹표준 사이트 만들기 : 코딩 : 이미지 유형01 / 웹사이트 만들기 / HTML 기초",
-        img: website17,
-        author: "webstoryboy",
-        videoId: "TpIIwvSF2bI",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[Official MV]비투비 BTOB(서은광,임현식,육성재) - 알듯 말듯해 Ambiguous [쌈,마이웨이OST Part.4]",
+        img: kpop17,
+        author: "RIAK OFFICIAL",
+        videoId: "9l5KoxFqQZY",
+        date: "2017.06.12",
+        channelId: "UC1XPu7aMRZaJsb-9PHMQt8g",
     },{
-        title: "18. 웹표준 사이트 만들기 : 코딩 : 이미지/텍스트 유형01 / 웹사이트 만들기 / CSS 기초",
-        img: website18,
-        author: "webstoryboy",
-        videoId: "gQxn_1RZeTw",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "순순희(윤지환), 잠골버스2기 - 그리워 그리워(feat. 잠골버스 준헌)",
+        img: kpop18,
+        author: "잠골버스",
+        videoId: "LPwehv2pb6M",
+        date: "2023.03.01",
+        channelId: "UCrZpcsm1OVzHRJswMxJ2lzg",
     },{
-        title: "19. 웹표준 사이트 만들기 : 코딩 : 카드 유형01 / 웹사이트 만들기 / CSS 강의 / HTML 강의",
-        img: website19,
-        author: "webstoryboy",
-        videoId: "R6aywQlO9Ok",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "[LIVE] 겨울이 지나고 봄이 오듯이 - 김나영 가로라이브",
+        img: kpop19,
+        author: "네버랜드 엔터테인먼트 NEVERLAND Ent.",
+        videoId: "E3gApE5TcBs",
+        date: "2021.03.14",
+        channelId: "UCPCiVJxr4GYS19-09UkfD7w",
     },{
-        title: "20. 웹표준 사이트 만들기 : 코딩 : 배너 유형01 / 웹사이트 만들기 / CSS 강의 / HTML 강의",
-        img: website20,
-        author: "webstoryboy",
-        videoId: "4cViXYJsAh0",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "SUNSHINE LOVE (Feat. SE O) (SUNSHINE LOVE (Feat. SE O (세오)))",
+        img: kpop20,
+        author: "STEN (스텐) - 주제",
+        videoId: "CMHV8JwjBNg",
+        date: "2022.05.23",
+        channelId: "UCXZo-r64InhHLdHujIBjODA",
     },{
-        title: "21. 웹표준 사이트 만들기 : 코딩 : 텍스트 유형01 / 웹사이트 만들기 / HTML / CSS / JAVASCRIPT",
-        img: website21,
-        author: "webstoryboy",
-        videoId: "4qQGji7V-GY",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
+        title: "처음처럼 사랑하길 바라고 있어",
+        img: kpop21,
+        author: "에스프레소 - 주제",
+        videoId: "loeiZoBpEZA",
+        date: "2022.10.14",
+        channelId: "UCdg5N8wIc1rKp_RnpiGUXMA",
     },{
-        title: "22. 웹표준 사이트 만들기 : 코딩 : 푸터 유형01  / 웹사이트 만들기 / HTML 웹사이트 만들기",
-        img: website22,
+        title: "Espresso (에스프레소) - 반쪽이라 말할 수 있어서 좋아 (Feat. 디아니)",
+        img: kpop22,
         author: "webstoryboy",
-        videoId: "crArpiJeLoU",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    },{
-        title: "22. 웹표준 사이트 만들기 : 코딩 : 푸터 유형01  / 웹사이트 만들기 / HTML 웹사이트 만들기",
-        img: website22,
-        author: "webstoryboy",
-        videoId: "crArpiJeLoU",
-        date: "2023.09.09",
-        channelId: "UCsvQSv7EeCMHyYb9ENKAJZw",
-    }
+        videoId: "N4W1cSBiITs",
+        date: "2013.12.23",
+        channelId: "UC02Eoi28drStFU5b4TXO-rw",
+    },
+    {
+        title: "박보람, 베이식 - 운명처럼 [맨투맨 OST Part.2] - 가사",
+        img: kpop23,
+        author: "딸구",
+        videoId: "il54yAri3jM",
+        date: "2017.04.28",
+        channelId: "UCC20cfzA-7diOE85NqQ62Yw",
+    },
+    {
+        title: "IU 'Love wins all' Live Clip (2024 IU WORLD TOUR CONCERT IN SEOUL)",
+        img: kpop24,
+        author: "이지금 [IU Official]",
+        videoId: "ax1csKKQnns",
+        date: "2024.03.29",
+        channelId: "UC3SyT4_WLHzN7JmHQwKQZww",
+    },
+    {
+        title: "[Official] '임재현 - 비의 랩소디' Live Clip",
+        img: kpop25,
+        author: "새벽테잎",
+        videoId: "xU6FRGi4lHw",
+        date: "2023.12.03",
+        channelId: "UCx8a_eqjwpl3sDH7YFmwkLQ",
+    },
 ]
